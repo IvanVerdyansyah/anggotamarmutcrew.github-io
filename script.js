24122c4649bbc870949d9f2a4984d22f3bb3df35
Ivan Verdyansyah <ivanverdyansyah80@gmail>
2021-10-22 16:11:21 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,83 @@
+var desk = '';
+//MEMASUKKAN NAMA
+	nama = prompt('Masukkan nama anda: Shanti, Dyah, Sulton, Kanea');
+
+	
+//MENANGKAP PILIHAN USER
+
+var konfir = true;
+	
+	if ( nama == 'Shanti' ) {
+		while ( konfir ) {
+				desk = alert('Orangnya perfectionism sekali');
+				desk = alert('Apa- apa bosenan');
+				desk = alert('Pantes jomblo wkwkw');
+				desk = alert('Kelakuannya cool kali jadi cewek kau');
+				desk = alert('Intinya jangan aja dikasi di kamar sendiri');
+				desk = alert('Nanti seperti video sebelumnya');
+				desk = alert('Lagi satu');
+				desk = alert('Cuma bisa maen basket');
+				desk = alert('Itupun kalo nyetak poin HOKI!');
+				desk = alert('Masih jagoan sinta maen pula wkwkwk');
+				desk = alert('Kalo ptm duduk di pojokan pula');
+				desk = alert('Introvert yaa?!');
+				desk = alert('Wkwkwkw');
+				konfir = confirm('Kurang jelas? Mau baca lagi? Apa mau keluar? Apa tunggu tak usir');
+			}
+
+	} else if ( nama == 'Dyah' ) {
+		while ( konfir ) {
+			desk = alert('Orangnya gendut sekali, sehat bugar dan rajin makan sampai maag');
+			desk = alert('Kalo sekolah selalu berangkat sendiri');
+			desk = alert('Gak pernah nebeng');
+			desk = alert('Mandiri sekali');
+			desk = alert('Baru masuk sekolah udah punya pacar');
+			desk = alert('Itu baru beberapa pertemuan udah 1 mantan');
+			desk = alert('Cobak bayangin dari awal udah sekolah behh');
+			desk = alert('Pacarannya langgeng jugak yaa?');
+			desk = alert('Lebih dari 1 tahun heran');
+			desk = alert('wkwkwkw');
+			konfir = confirm('Kurang jelas? mau baca lagi? apa mau keluar? apa tunggu tak usir');
+		}
+		
+	} else if ( nama == 'Sulton' ) {
+		while ( konfir ) {
+			desk = alert('Orangnya alim dan rajin solat');
+			desk = alert('Disiplin waktu solat sekali');
+			desk = alert('Posisi diperjalanan pun solat');
+			desk = alert('Astagfirullah hallazdimm... Hambamu yaallah');
+			desk = alert('Jago kali ngoding sampe lancar ngopas');
+			desk = alert('ehh');
+			desk = alert('Gak ada ceweknya ni padahal ganteng naudhubillah minzdalik');
+			desk = alert('Kalo masalah cewek jangan ditanya udahh');
+			desk = alert('Kalo ditanya jawabannya pasti "yang penting durasi boss"');
+			desk = alert('Wkwkwkw');
+			konfir = confirm('Kurang jelas? mau baca lagi? apa mau keluar? apa tunggu tak usir');
+		}
+		
+	} else if ( nama == 'Kanea' ) {
+		while ( konfir ) {
+			desk = alert('Orangnya jomblo dari lahir as single sejak dini');
+			desk = alert('Hp aja sempet dipinjemin dari pacarnya');
+			desk = alert('Tunggu aja nanti tv disana dikasi pinjem lagi');
+			desk = alert('Punya hp bagus kali pasti sudah di bor-up');
+			desk = alert('Bolak balik dari service kann??');
+			desk = alert('Lain kali kalo ada uang beliin mainan lego atau apa untuk adek sendiri');
+			desk = alert('Jangan sampek kurang mainan dirumah laptop dijadiin mainan');
+			desk = alert('Itu laptop kayak lego udah bisa di bongkar pasang');
+			desk = alert('Wkwkwkw');
+			konfir = confirm('Kurang jelas? mau baca lagi? apa mau keluar? apa tunggu tak usir');
+		}
+		
+	} else {
+
+		desk = alert('Nama yang anda masukkan tidak terdaftar di grup MARMUT');
+	}
+
+
+
+//HASIL NAMA
+alert('Cuma bercanda anj*** jangan baperan cuk, cuma disuruh jadi user aja');
+alert('Belum disuruh jadi babu sekolah kau!')
+alert('Sekian terimakasih');
+alert('Terima Gaji BOSS!!');
